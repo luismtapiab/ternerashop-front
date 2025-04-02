6e41f2d19ff960fcd4b7b806a17f5c1f111c225b
Luis Tapia <lmtapia1024@gmail.com>
2025-04-01 23:54:40 -0400

    starting with product details page

--- src/app/pages/product-list/product-card/product-card.component.ts
+++ src/app/pages/product-list/product-card/product-card.component.ts
@@ -4,29 +4,34 @@ import { PrimaryButtonComponent } from "../../../components/primary-button/prima
 import { CartService } from '../../../services/cart.service';
 import { decimal2 } from '../../../../utils';
 import { environment } from '../../../../environments/environment';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-product-card',
-  imports: [PrimaryButtonComponent],
+  imports: [PrimaryButtonComponent,RouterLink],
   template: `
-    <div class="card">
-        <img src={{product().image}} />
-    <div class="details">
-        <p class="name">{{product().name}}</p>
-        <p class="price">{{priceString()}} Bs</p>
-    </div>
     
-    @if (cartService.show) {
-        <app-primary-button label="Add to Cart" (btnClicked)="cartService.addToCart(product())"/>
-    }
+        <div class="card" [routerLink]="['/product', product().id]"
+          [state]="{ justProduct : product()}">
+            <img src={{product().image}} />
+        <div class="details">
+            <p class="name">{{product().name}}</p>
+            <p class="price">{{priceString()}} Bs</p>
+        </div>
+        
+        @if (cartService.show) {
+            <app-primary-button label="Add to Cart" (btnClicked)="cartService.addToCart(product())"/>
+        }
+        
+        <span class="stock" 
+             [class]="product().stock ? 'text-green' : 'text-red'"> 
+            @if (product().stock) {
+                {{product().stock}} left
+            } @else { Out of <br> stock }
+        </span>
+        </div>
     
-    <span class="stock" 
-       [class]="product().stock ? 'text-green' : 'text-red'"> 
-        @if (product().stock) {
-            {{product().stock}} left
-        } @else { Out of <br> stock }
-    </span>
-    </div>
     `,
   styles: `
   .card {
