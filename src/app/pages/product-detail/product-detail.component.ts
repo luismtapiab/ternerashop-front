8ef5d8d041222cdb93e1f052ded0e3178d66b808
Luis Tapia <lmtapia1024@gmail.com>
2025-04-11 22:48:22 -0400

    primary-button bubbling preventedwith stopPropagation

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,32 +1,63 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { Product } from '../../models/product.model';
-import { JsonPipe } from '@angular/common';
 import { decimal2 } from '../../../utils';
+import { CartService } from '../../services/cart.service';
+import { PrimaryButtonComponent } from "../../components/primary-button/primary-button.component";
 
 @Component({
   selector: 'app-product-detail',
-  imports: [JsonPipe],
+  imports: [PrimaryButtonComponent],
   template: `
-    <h2>
-      product-detail works!
-    </h2>
-
-    <strong> {{product | json}}
-    </strong>
-
-    <div class="card">
-        <img src={{product().image}} />
-        <div class="details">
-            <p class="name">{{product().name}}</p>
-            <p class="price">{{priceString()}} Bs</p>
+    <div class="card two-columns">
+        <div class="product">
+            <img src={{product.image}} />
+        
+        @if (cartService.show) {
+            <app-primary-button label="Add to Cart" 
+            (btnClicked)="this.cartService.addToCart(this.product)"/>
+        }
+        
+        <span class="stock" 
+             [class]="product.stock ? 'text-green' : 'text-red'"> 
+            @if (product.stock) {
+                {{product.stock}} left
+            } @else { Out of <br> stock }
+        </span>
         </div>
+        <div>
+            <h2>{{product.name}}</h2>
+            <p class="price">{{priceString()}} Bs</p>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos velit totam, nobis ut et fugiat excepturi deleniti commodi assumenda quod voluptates, facere quas, doloribus praesentium quia unde obcaecati quo illo.
+            </p>
+        </div>    
     </div>
-  `,
-  styles: ``
+  `
+  ,
+  styles: `
+    .two-columns {
+        display: grid;
+        grid-template-columns: 1fr 2fr;
+    }
+
+
+    .product {
+        display: flex;
+        padding: 6px;
+        justify-content: center;
+        gap: 1rem;
+        align-items: center;
+        flex-direction: column;
+    }
+    .price {
+        font-size: 1.5rem;
+        font-style: strong;
+    }
+  `
 })
 export class ProductDetailComponent {
+    cartService = inject(CartService)
     router = inject(Router)
     product = this.router.getCurrentNavigation()?.extras.state?.['justProduct']; 
-    priceString = computed(()=>decimal2(this.product().price))
+    priceString = computed(()=>decimal2(this.product.price))
+
 }
